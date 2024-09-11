8915e11540a0e65db55165bb7f5a7ab34ec56a58
aliefadam <aliefadam6@gmail.com>
2024-09-11 17:27:59 +0700

    pengerjaan modal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,42 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.scss";
 
-console.log('Hello Coders! :)');
+const menuContainer = document.getElementById("menu-container");
+document.addEventListener("click", ({ target }) => {
+  if (target.classList.contains("btn-lihat-detail")) {
+    // alert("asd");
+  }
+});
+
+const show = () => {
+  fetch("/data/DATA.json")
+    .then((res) => res.json())
+    .then((res) => {
+      const restaurants = res.restaurants;
+      let HTML = "";
+      restaurants.forEach((restaurant, i) => {
+        HTML += `
+        <div class="menu-item" restauran-id="${restaurant.id}">
+          <div class="menu-image">
+            <img
+              src="${restaurant.pictureId}"
+              alt="Gambar Restoran ${restaurant.name}"
+            />
+            <div class="label-city poppins-medium">${restaurant.city}</div>
+          </div>
+          <div class="menu-detail">
+            <p>Rating ${restaurant.rating} <i class="fa-solid fa-star"></i></p>
+            <h3>${restaurant.name}</h3>
+            <p class="poppins-light desc">${restaurant.description
+              .toString()
+              .substring(0, 100)}...</p>
+            <button class="btn-lihat-detail">Lihat Detail</button>
+          </div>
+        </div>
+        `;
+      });
+      menuContainer.innerHTML = HTML;
+    });
+};
+
+show();
